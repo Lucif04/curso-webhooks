f77e4c0dd705358435dc1266373e8a35475ce608
Luci <bellomelucia@gmail.com>
2025-03-16 00:47:13 -0300

    Agregamos comentarios

--- src/presentation/github/controller.ts
+++ src/presentation/github/controller.ts
@@ -3,14 +3,16 @@ import { GitHubService } from "../services/github.service";
 import { DiscordService } from "../services/discord.service";
 
 export class GithubController {
-  // Estas son las inyecciones de dependencia 
+  // Estas son las inyecciones de dependencia
   constructor(
     private readonly githubService = new GitHubService(),
     private readonly discordService = new DiscordService()
   ) {}
 
   webhookHandler = (req: Request, res: Response) => {
+    // 'x-' -> Significa que es un header personalizado, en este caso nos dice cual es el evento.
     const githubEvent = req.header("x-github-event") ?? "unknown";
+    // payload: es la parte de un mensaje o paquete de datos que contiene la información que se quiere transmitir
     const payload = req.body;
     let message: string;
 
